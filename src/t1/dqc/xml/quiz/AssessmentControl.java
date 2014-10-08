426c05e83894d0ac1827bc36259e78724f756201
Jason <jaso66@gmail.com>
2014-10-08 16:10:05 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/TestZipAndManifest.java

---
+++ src/t1/dqc/xml/quiz/AssessmentControl.java
@@ -0,0 +1,45 @@
+package t1.dqc.xml.quiz;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "assessmentcontrol")
+public class AssessmentControl
+{
+	private String feedbackswitch;
+	private String solutionswitch;
+	private String hintswitch;
+	
+	@XmlAttribute(name = "feedbackswitch")
+	public String getFeedbackswitch()
+	{
+		return feedbackswitch;
+	}
+
+	public void setFeedbackswitch(String feedbackswitch)
+	{
+		this.feedbackswitch = feedbackswitch;
+	}
+
+	@XmlAttribute(name = "solutionswitch")
+	public String getSolutionswitch()
+	{
+		return solutionswitch;
+	}
+
+	public void setSolutionswitch(String solutionswitch)
+	{
+		this.solutionswitch = solutionswitch;
+	}
+	
+	@XmlAttribute(name = "hintswitch")
+	public String getHintswitch()
+	{
+		return hintswitch;
+	}
+
+	public void setHintswitch(String hintswitch)
+	{
+		this.hintswitch = hintswitch;
+	}	
+}
