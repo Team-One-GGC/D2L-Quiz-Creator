bfedfd1ccde0f8bfd9d481c8ee7b6d50a14d1d8b
Derek Cofer <dcofer@ggc.edu>
2014-10-12 13:56:08 -0400

    Started work on options package and Procextension class

---
+++ src/t1/dqc/xml/quiz/options/Procextension.java
@@ -0,0 +1,19 @@
+package t1.dqc.xml.quiz.options;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "assess_procextension")
+public class Procextension
+{
+    private IntroMessage message;
+    private String disableRightClick = "no";
+    private String disablePagerAccess = "no";
+    private String isActive;
+    //Start Date goes here
+    //End Date goes here    
+    private String hasSceduleEvent = "no";
+    private String password;
+    private String isAttemptRldb = "no";
+    private String isSubviewRldb = "no";
+    private int timeLimit;
+}
