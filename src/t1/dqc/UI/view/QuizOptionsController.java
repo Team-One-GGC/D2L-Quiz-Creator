20403116fd3ad2963cc2866336266cf28ca8a9ba
Jared Marquez <smoe79@gmail.com>
2014-10-28 16:26:38 -0400

    Updated QuizOptionController
    Added empty methods for saving, canceling, and setting. Added AM/PM
    combo boxes
    Tried to add text limiter function to time fields.

--- src/t1/dqc/UI/view/QuizOptionsController.java
+++ src/t1/dqc/UI/view/QuizOptionsController.java
@@ -25,6 +25,8 @@ public class QuizOptionsController {
     private TextField startHour;
     @FXML
     private TextField startMin;
+    @FXML
+    private ComboBox<String> startAMPM;
     
     //Quiz End Date
     @FXML
@@ -35,6 +37,8 @@ public class QuizOptionsController {
     private TextField endHour;
     @FXML
     private TextField endMin;
+    @FXML
+    private ComboBox<String> endAMPM;
     
     //Quiz Password
     @FXML
@@ -59,6 +63,9 @@ public class QuizOptionsController {
     //Value of Quiz Status
     private String quizStatus;
     
+    //Limit of time fields
+    private final int LIMIT = 2;
+    
     
     //Constructor for Quiz Options Controller
     public QuizOptionsController(){
@@ -70,6 +77,11 @@ public class QuizOptionsController {
         //Initialize logic for combo box
         statusChoice.getItems().clear();
         statusChoice.getItems().addAll("Enabled","Disabled");
+        startAMPM.getItems().clear();
+        startAMPM.getItems().addAll("AM", "PM");
+        endAMPM.getItems().clear();
+        endAMPM.getItems().addAll("AM", "PM");
+        setFields();
     }
     
     //Selecting Quiz Status
@@ -78,6 +90,33 @@ public class QuizOptionsController {
        System.out.println("Quiz status is " + quizStatus);
     }
     
+    //@FXML
+    //Limit Text in Time Fields
+   /* public void timeFieldLimiter(){
+        if (timeField.getText().length() >= LIMIT) {
+
+            // if it's 11th character then just setText to previous
+            // one
+            timeField.setText(timeField.getText().substring(0, LIMIT));
+        }*/
+    //}
     
+    //Set fields to Quiz values
+    @FXML
+    public void setFields(){
+    
+    }
+    
+    //Save values to Quiz Object
+    @FXML
+    public void saveValues(){
+        
+    }
+    
+    //Cancel Changes and close stage
+    @FXML
+    public void cancelSaves(){
+        
+    }
     
 }
