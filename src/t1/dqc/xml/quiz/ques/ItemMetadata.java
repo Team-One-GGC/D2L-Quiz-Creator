4e92bbe2801eaa63784e6193b697794bbbbc9250
Jason <jaso66@gmail.com>
2014-10-29 16:47:42 -0400

    Question Metadata
    Added Question Metadata

--- src/t1/dqc/xml/quiz/ques/ItemMetadata.java
+++ src/t1/dqc/xml/quiz/ques/ItemMetadata.java
@@ -1,6 +1,40 @@
 package t1.dqc.xml.quiz.ques;
 
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "itemmetadata")
 public class ItemMetadata
 {
+	/**
+	 * @param list
+	 */
+    public ItemMetadata(List<QtiMetadataField> list)
+    {
+	    super();
+	    this.list = list;
+	    QtiMetadataField item1 = new QtiMetadataField();
+	    item1.setFieldlabel("qmd_computerscored");
+	    item1.setFieldentry("yes");
+	    list.add(item1);
+	    
+	    QtiMetadataField item2 = new QtiMetadataField();
+	    item1.setFieldlabel("qmd_questiontype");
+	    item1.setFieldentry("True/False");
+	    list.add(item2);
+	    
+	    QtiMetadataField item3 = new QtiMetadataField();
+	    item1.setFieldlabel("qmd_weighting");
+	    item1.setFieldentry("1");
+	    list.add(item3);
+    }
 
+	@XmlElementWrapper(name = "qtimetadata")
+	@XmlElement(name = "qtimetadatafield")
+	private List<QtiMetadataField> list;
 }
+
+
---
+++ src/t1/dqc/xml/quiz/ques/QtiMetadataField.java
@@ -0,0 +1,36 @@
+package t1.dqc.xml.quiz.ques;
+
+import javax.xml.bind.annotation.*;
+
+
+
+
+
+@XmlRootElement(name = "qtimetadatafield")
+public class QtiMetadataField
+{
+	private String fieldlabel;
+	private String fieldentry;
+	
+	@XmlElement(name = "fieldlabel")
+	public String getFieldlabel()
+	{
+		return fieldlabel;
+	}
+	public void setFieldlabel(String fieldlabel)
+	{
+		this.fieldlabel = fieldlabel;
+	}
+	
+	@XmlElement(name = "fieldentry")
+	public String getFieldentry()
+	{
+		return fieldentry;
+	}
+	public void setFieldentry(String fieldentry)
+	{
+		this.fieldentry = fieldentry;
+	}
+	
+	
+}
