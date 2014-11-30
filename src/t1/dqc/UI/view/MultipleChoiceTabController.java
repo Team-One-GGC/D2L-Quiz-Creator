b1ce1fe0e6b7b4783c1794a5131e7d24d457b0cd
Jared Marquez <smoe79@gmail.com>
2014-11-30 14:15:21 -0500

    Fixing question tab page

---
+++ src/t1/dqc/UI/view/MultipleChoiceTabController.java
@@ -0,0 +1,19 @@
+package t1.dqc.UI.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
+
+public class MultipleChoiceTabController {
+
+       @FXML
+       private Tab multipleChoiceTab;
+       
+       @FXML
+       private ScrollPane multipleChoicePane;
+       
+       @FXML
+       private AnchorPane multipleChoiceAPane;
+}
