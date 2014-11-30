0122b596427014c407b032f0266a236ef5c7312c
Jared Marquez <smoe79@gmail.com>
2014-11-30 15:54:46 -0500

    Added LongAnswer to view w/ basic controller

--- src/t1/dqc/UI/view/LongAnswerTabController.java
+++ src/t1/dqc/UI/view/LongAnswerTabController.java
@@ -1,5 +1,30 @@
 package t1.dqc.UI.view;
 
+import t1.dqc.UI.MainQuizCreator;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LongAnswerTabController {
 
+    @FXML
+    private TextField titleName;
+    
+    @FXML
+    private TextField points;
+    
+    @FXML
+    private ChoiceBox<String> difficulty;
+    
+    @FXML
+    private TextArea questionText;
+    
+    @FXML
+    private TextArea inputText;
+    
 }
