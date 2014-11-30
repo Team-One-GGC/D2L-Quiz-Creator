86e6a7e67486f1cf5cf1d5158356a71d57d9d7af
Jared Marquez <smoe79@gmail.com>
2014-11-30 14:36:49 -0500

    Both tab panes with second version

---
+++ src/t1/dqc/UI/view/TrueFalseTabController.java
@@ -0,0 +1,17 @@
+package t1.dqc.UI.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
+
+public class TrueFalseTabController {
+
+    @FXML
+    private Tab trueFalseTab;
+    
+    @FXML
+    private AnchorPane trueFalsePane;
+        
+}
