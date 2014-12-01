108930589a81d269143013e07b8a65d48c045e76
Derek Cofer <dcofer@ggc.edu>
2014-12-01 16:29:06 -0500

    Merged in new tab controllers

--- src/t1/dqc/UI/view/CreateQuestionsController.java
+++ src/t1/dqc/UI/view/CreateQuestionsController.java
@@ -1,23 +1,16 @@
 package t1.dqc.UI.view;
 
-import java.util.List;
-
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import t1.dqc.UI.MainQuizCreator;
 import t1.dqc.xml.quiz.Quiz;
-import t1.dqc.xml.quiz.ques.Question;
-import t1.dqc.xml.util.QuestionFactory;
-import t1.dqc.xml.util.QuizFactory;
 
 public class CreateQuestionsController
 {
 	//Reference to the main D2LQuizCreation application
 	private MainQuizCreator mainQuizCreator;
-			
+	
+	@FXML
+	private TrueFalseTabController trueFalsePaneController;
 	
 	
 	//Constructor for Create Questions Controller
@@ -28,22 +21,9 @@ public class CreateQuestionsController
 	
 	public void setFields(Quiz quiz)
 	{
-	    List<Question> questions = QuizFactory.getQuestions(quiz);
-	    titleNameTF.setText(QuestionFactory.getQuestionTitle(questions.get(0)));
-	    pointsTF.setText(QuestionFactory.getQuestionPoints(questions.get(0)));
-	    
-	    difficultyAmtTF.setValue(QuestionFactory.getQuesitonDiff(questions.get(0)));
-	    
-	    questionTextTF.setText(QuestionFactory.getQuestionText(questions.get(0)));
-	    
-	    boolean tfAns = QuestionFactory.getTFAnswer(questions.get(0));
-	    if(tfAns)
-	        trueAnswer.setSelected(true);
-	    else
-	        falseAnswer.setSelected(true);
-	        
+	    trueFalsePaneController.setFields(quiz);
 	}
-	
+		
     //Setter
 	public void setMainQuizCreator(MainQuizCreator mainQuizCreator)
 	{
--- src/t1/dqc/UI/view/TrueFalseTabController.java
+++ src/t1/dqc/UI/view/TrueFalseTabController.java
@@ -1,6 +1,12 @@
 package t1.dqc.UI.view;
 
+import java.util.List;
+
 import t1.dqc.UI.MainQuizCreator;
+import t1.dqc.xml.quiz.Quiz;
+import t1.dqc.xml.quiz.ques.Question;
+import t1.dqc.xml.util.QuestionFactory;
+import t1.dqc.xml.util.QuizFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
@@ -11,7 +17,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class TrueFalseTabController {
+public class TrueFalseTabController extends AnchorPane{
 
   //Reference to the main D2LQuizCreation application
     private MainQuizCreator mainQuizCreator;
@@ -44,5 +50,23 @@ public class TrueFalseTabController {
   //TrueFalse Question text    
     @FXML
     private TextArea questionText;
+    
+    public void setFields(Quiz quiz)
+    {
+        List<Question> questions = QuizFactory.getQuestions(quiz);
+        titleName.setText(QuestionFactory.getQuestionTitle(questions.get(0)));
+        points.setText(QuestionFactory.getQuestionPoints(questions.get(0)));
+        
+        difficulty.setValue(QuestionFactory.getQuesitonDiff(questions.get(0)));
+        
+        questionText.setText(QuestionFactory.getQuestionText(questions.get(0)));
+        
+        boolean tfAns = QuestionFactory.getTFAnswer(questions.get(0));
+        if(tfAns)
+            trueAnswer.setSelected(true);
+        else
+            falseAnswer.setSelected(true);
+            
+    }
     
 }
